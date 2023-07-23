0ea8777aa61a482ce7def1d768e9f8512d797d71
CatrielNanthaveth <91494648+CatrielNanthaveth@users.noreply.github.com>
2023-07-23 15:43:28 -0300

    setup inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { ErrorPage, Root } from "./routes";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      
+    ]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
-
 export default App;
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ErrorPage = () => {
+    return (
+        <>
+            <h1>
+                error 404 not found
+            </h1>
+        </>
+    )
+}
