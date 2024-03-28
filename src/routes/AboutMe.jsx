9c966fc2fd830ca4f3d54b9345314cc0ceb34c64
CatrielNanthaveth <catriel_nanthaveth@hotmail.com>
2024-03-28 01:12:15 -0300

    refactoring

--- src/routes/AboutMe.jsx
+++ src/routes/AboutMe.jsx
@@ -3,78 +3,59 @@ import { Card, Carousel, Space, Tag } from 'antd'
 import React from 'react'
 
 export const AboutMe = () => {
+
+    const data = [
+        {
+            name: "About me",
+            extra: <Space>
+                <a href="https://github.com/CatrielNanthaveth" rel="noreferrer" target="_blank"><GithubOutlined /></a>
+                <a href="https://www.linkedin.com/in/catriel-nanthaveth/" rel="noreferrer" target="_blank"><LinkedinOutlined /></a>
+                <a href="mailto:catriel_nanthaveth@hotmail.com" rel="noreferrer" target="_blank"><MailOutlined /></a>
+            </Space>,
+            description: ["I'm a student of Computer Science at Universidad Nacional del Oeste in Merlo, Buenos Aires.", "I love learning about things that interest me, like technology or video games.", "I am a sociable person and I like to generate comfort in my work environment because that way I am more efficient."],
+            tags: ["Adaptability", "Communication", "Team Work", "Problem Solving", "Compromise"]
+        },
+        {
+            name: "Backend",
+            extra: null,
+            description: ["I have a year learning backend technologies.", "I use this knowledge to do my job as a Web Programmer in Shalion.", "I'm always looking for deep my learn as a backend developer."],
+            tags: ["NodeJS", "Express", "PostgreSQL", "Postman", "REST API", "GraphQL", "OAuth", "JWT", "other libraries & languages"]
+        },
+        {
+            name: "Frontend",
+            extra: null,
+            description: ["I have about two years learning frontend technologies.", "I was in a bootcamp where I learnt ReactJS and libraries related."],
+            tags: ["ReactJS", "NextJS", "HTML", "CSS", "SaSS", "Redux", "other libraries"]
+        },
+        {
+            name: "Testing",
+            extra: null,
+            description: ["While developing my projects, I have to test it, so there are some technologies that I use.", "I don't use all of these, because my projects aren't big enough, so I'm not fluent in some of these."],
+            tags: ["Postman", "Jest", "JUnit", "TDD", "PlayWright"]
+        },
+    ]
     return (
         <div className='projects'>
             <Carousel>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>About me</h1>}
-                        extra={
-                            <Space>
-                                <a href="https://github.com/CatrielNanthaveth" rel="noreferrer" target="_blank"><GithubOutlined /></a>
-                                <a href="https://www.linkedin.com/in/catriel-nanthaveth/" rel="noreferrer" target="_blank"><LinkedinOutlined /></a>
-                                <a href="mailto:catriel_nanthaveth@hotmail.com" rel="noreferrer" target="_blank"><MailOutlined /></a>
-                            </Space>}
-                    >
-                        <p className="card-text">I'm a student of Computer Science at Universidad Nacional del Oeste in Merlo, Buenos Aires.</p>
-                        <p className="card-text">I love learning about things that interest me, like technology or video games.</p>
-                        <p className="card-text">I am a sociable person and I like to generate comfort in my work environment because that way I am more efficient.</p>
+
+                {data.map(e => {
+                    return <div>
+                        <Card className="project-card"
+                            title={<h1>{e.name}</h1>}
+                            extra={e.extra}
+                        >
+                            {e.description.map(paragraph => {
+                                return <p className='card-text'>{paragraph}</p>
+                            })}
 
 
-                        <Tag className="project-tag">Adaptability</Tag>
-                        <Tag className="project-tag">Communication</Tag>
-                        <Tag className="project-tag">Team Work</Tag>
-                        <Tag className="project-tag">Problem-solving</Tag>
-                        <Tag className="project-tag">Compromise</Tag>
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>Backend</h1>}
-                    >
-                        <p className="card-text">I have about half a year learning backend technologies.</p>
-                        <p className="card-text">I'd like to focus in this side of the web development, but my frontend skills are better for now.</p>
-                        <p className="card-text">In any case, I like to learn and improve my skills.</p>
+                            {e.tags.map(tag => {
+                                return <Tag className="project-tag">{tag}</Tag>
+                            })}
 
-
-                        <Tag className="project-tag">Javascript</Tag>
-                        <Tag className="project-tag">NodeJS</Tag>
-                        <Tag className="project-tag">Express</Tag>
-                        <Tag className="project-tag">MongoDB</Tag>
-                        <Tag className="project-tag">PostgreSQL</Tag>
-                        <Tag className="project-tag">other libraries & languages</Tag>
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>Frontend</h1>}
-                    >
-                        <p className="card-text">I have about one year learning frontend technologies.</p>
-                        <p className="card-text">I was in a bootcamp where I learnt ReactJS and libraries related.</p>
-
-                        <Tag className="project-tag">Javascript</Tag>
-                        <Tag className="project-tag">ReactJS</Tag>
-                        <Tag className="project-tag">HTML</Tag>
-                        <Tag className="project-tag">CSS</Tag>
-                        <Tag className="project-tag">SaSS</Tag>
-                        <Tag className="project-tag">Redux</Tag>
-                        <Tag className="project-tag">other libraries</Tag>
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>Testing</h1>}
-                    >
-                        <p className="card-text">While developing my projects, I have to test it, so there are some technologies that I use.</p>
-                        <p className="card-text">I don't use all of these, because my projects aren't big enough, so I'm not fluent in some of these.</p>
-
-                        <Tag className="project-tag">Postman</Tag>
-                        <Tag className="project-tag">Jest</Tag>
-                        <Tag className="project-tag">JUnit</Tag>
-                        <Tag className="project-tag">TDD</Tag>
-
-                    </Card>
-                </div>
+                        </Card>
+                    </div>
+                })}
 
             </Carousel>
         </div>
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -2,79 +2,53 @@ import { Card, Carousel, Tag } from "antd";
 
 export const Projects = () => {
 
+    const projects = [
+        {
+            name: "TODO List",
+            link: "https://github.com/CatrielNanthaveth/todo-app-with-backend",
+            description: ["Project made for the subject of Interfaces de Usuario y Tecnologías Web.", "Web application that allows you to create, edit, complete and delete pending tasks.", "To do this, you can log in and register on the page, allowing the tasks to be stored on the web and not lost once you leave the page."],
+            tags: ["NodeJS", "ReactJS", "Express", "Postgres"]
+        },
+        {
+            name: "Random Quote Machine",
+            link: "https://github.com/CatrielNanthaveth/Random-Quote-Machine",
+            description: ["Project made for the React course of FreeCodeCamp.org.", "Web application that allows obtaining a random quote.", "This phrase can be shared on Twitter through a simple button."],
+            tags: ["HTML", "CSS", "Javascript", "ReactJS"]
+        },
+        {
+            name: "Pong!",
+            link: "https://github.com/CatrielNanthaveth/Pong",
+            description: ["Project made for the subject of Programación Orientada a Objetos 2.", "Python game that imitates Pong!.", "It has a simple page that explain the project."],
+            tags: ["Python", "Pygame", "HTML", "CSS"]
+        },
+        {
+            name: "NGO web",
+            link: "https://github.com/alkemyTech/React-SXXI-T2",
+            description: ["Project made for the React bootcamp dictated by the Universidad Siglo 21.", "Landing Page of a NGO.", "We did it with the Scrum methodology and a five members team + a teacher who work as Scrum Master."],
+            tags: ["ReactJS", "Axios", "Redux", "HTML", "SaSS"]
+        },
+    ]
+
     return (
         <div className='projects'>
             <Carousel>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>TODO List</h1>}
-                        extra={<a href="https://github.com/CatrielNanthaveth/todo-app-with-backend" rel="noreferrer" target="_blank">Github</a>}
-                    >
-                        <p className="card-text">Project made for the subject of Interfaces de Usuario y Tecnologías Web.</p>
-                        <p className="card-text">Web application that allows you to create, edit, complete and delete pending tasks.</p>
-                        <p className="card-text">To do this, you can log in and register on the page, allowing the tasks to be stored on the web and not lost once you leave the page.</p>
+                {projects.map(e => {
+                    return <div>
+                        <Card className="project-card"
+                            title={<h1>{e.name}</h1>}
+                            extra={<a href={e.link} rel="noreferrer" target="_blank">Github</a>}
+                        >
+                            {e.description.map(paragraph => {
+                                return <p className="card-text">{paragraph}</p>
+                            })}
 
+                            {e.tags.map(tag => {
+                                return  <Tag className="project-tag">{tag}</Tag>
+                            })}
 
-                        <Tag className="project-tag">NodeJS</Tag>
-                        <Tag className="project-tag">ReactJS</Tag>
-                        <Tag className="project-tag">Express</Tag>
-                        <Tag className="project-tag">Postgres</Tag>
-
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>Random Quote Machine</h1>}
-                        extra={<a href="https://github.com/CatrielNanthaveth/Random-Quote-Machine" rel="noreferrer" target="_blank">Github</a>}
-                    >
-                        <p className="card-text">Project made for the React course of FreeCodeCamp.org.</p>
-                        <p className="card-text">Web application that allows obtaining a random quote.</p>
-                        <p className="card-text">This phrase can be shared on Twitter through a simple button.</p>
-
-
-                        <Tag className="project-tag">HTML</Tag>
-                        <Tag className="project-tag">CSS</Tag>
-                        <Tag className="project-tag">Javascript</Tag>
-                        <Tag className="project-tag">ReactJS</Tag>
-
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>Pong!</h1>}
-                        extra={<a href="https://github.com/CatrielNanthaveth/Pong" rel="noreferrer" target="_blank">Github</a>}
-                    >
-                        <p className="card-text">Project made for the subject of Programación Orientada a Objetos 2.</p>
-                        <p className="card-text">Python game that imitates Pong!.</p>
-                        <p className="card-text">It has a simple page that explain the project.</p>
-
-
-                        <Tag className="project-tag">Python</Tag>
-                        <Tag className="project-tag">Pygame</Tag>
-                        <Tag className="project-tag">HTML</Tag>
-                        <Tag className="project-tag">CSS</Tag>
-
-                    </Card>
-                </div>
-                <div>
-                    <Card className="project-card"
-                        title={<h1>NGO web</h1>}
-                        extra={<a href="https://github.com/alkemyTech/React-SXXI-T2" rel="noreferrer" target="_blank">Github</a>}
-                    >
-                        <p className="card-text">Project made for the React bootcamp dictated by the Universidad Siglo 21.</p>
-                        <p className="card-text">Landing Page of a NGO.</p>
-                        <p className="card-text">We did it with the Scrum methodology and a five members team + a teacher who work as Scrum Master.</p>
-
-
-                        <Tag className="project-tag">ReactJS</Tag>
-                        <Tag className="project-tag">Axios</Tag>
-                        <Tag className="project-tag">Redux</Tag>
-                        <Tag className="project-tag">HTML</Tag>
-                        <Tag className="project-tag">CSS</Tag>
-
-                    </Card>
-                </div>
-
+                        </Card>
+                    </div>
+                })}
             </Carousel>
         </div>
     )
